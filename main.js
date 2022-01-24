68c1a7598e7453236896c17344ba66b1b80e2396
Red1082 <arosu1082@gmail.com>
2022-01-24 22:06:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ const canvas = new Canvas({
 graph.appendFunc('log', Math.log2, 'green');
 graph.appendFunc('n', n => n < 0 ? NaN : n, '#44f902');
 graph.appendFunc('nlogn', n => n * Math.log2(n), '#ddf902');
-// graph.appendFunc('n^2', n => n < 0 ? NaN : n ** 2, '#fc1500');
+graph.appendFunc('n^2', n => n < 0 ? NaN : n ** 2, '#fc1500');
 // */
 
 function draw() {
